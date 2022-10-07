c24fa08ba930aa51d4abcc5fbf66c3e65624ca51
Comteul <herbwilly09@gmail.com>
2022-10-07 16:24:20 +0900

    code edit: 20221007 - fin

--- js/background.js
+++ js/background.js
@@ -1,18 +1,18 @@
-// const images = [
-//     "bg2.png",
-//     "bg3.png",
-//     "bg4.png",
-//     "bg5.png",
-//     "bg6.png",
-//     "bg7.png",
-//     "bg8.png",
-//     "bg9.png",
-//     "bg10.png",
-//     "bg11.png",
-//     "bg12.png",
-//     "bg13.png",
-// ];
+const images = [
+    "bg2.png",
+    "bg3.png",
+    "bg4.png",
+    "bg5.png",
+    "bg6.png",
+    "bg7.png",
+    "bg8.png",
+    "bg9.png",
+    "bg10.png",
+    "bg11.png",
+    "bg12.png",
+    "bg13.png",
+];
 
-// const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-// document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
